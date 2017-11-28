d33a0c9250099eff6344f31c2ba626b91b5a0087
Grzegorz Szymaszek <gszymaszek@short.pl>
2017-11-28 13:34:53 +0100

    Moved $_SESSION conversion logic
    Moved code that conditionally converts data fetched from $_SESSION to
    plain array a few lines up for better readability.

--- src/SessionManager.php
+++ src/SessionManager.php
@@ -126,17 +126,17 @@ class SessionManager extends AbstractManager
         $oldSessionData = [];
         if (isset($_SESSION)) {
             $oldSessionData = $_SESSION;
+
+            // convert session data to plain array that’ll be acceptable as
+            // ArrayUtils::merge parameter
+            if ($oldSessionData instanceof Storage\StorageInterface) {
+                $oldSessionData = $oldSessionData->toArray();
+            } elseif ($oldSessionData instanceof \Traversable) {
+                $oldSessionData = iterator_to_array($oldSessionData);
+            }
         }
 
         session_start();
-
-        // convert session data to plain array that’ll be acceptable as
-        // ArrayUtils::merge parameter
-        if ($oldSessionData instanceof Storage\StorageInterface) {
-            $oldSessionData = $oldSessionData->toArray();
-        } elseif ($oldSessionData instanceof \Traversable) {
-            $oldSessionData = iterator_to_array($oldSessionData);
-        }
 
         if (! empty($oldSessionData) && is_array($oldSessionData)) {
             $_SESSION = ArrayUtils::merge($oldSessionData, $_SESSION, true);
