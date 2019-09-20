b0b4baabe004cdbc09bd86a4afffe52c32b17758
webimpress <contact@webimpress.com>
2019-09-20 10:38:56 +0100

    Update doc headers

--- test/SessionManagerTest.php
+++ test/SessionManagerTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @see       https://github.com/zendframework/zend-session for the canonical source repository
- * @copyright Copyright (c) 2005-2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @copyright Copyright (c) 2005-2019 Zend Technologies USA Inc. (https://www.zend.com)
  * @license   https://github.com/zendframework/zend-session/blob/master/LICENSE.md New BSD License
  */
 
--- test/autoload.php
+++ test/autoload.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @see       https://github.com/zendframework/zend-mail for the canonical source repository
- * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-mail/blob/master/LICENSE.md New BSD License
+ * @see       https://github.com/zendframework/zend-session for the canonical source repository
+ * @copyright Copyright (c) 2017-2019 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-session/blob/master/LICENSE.md New BSD License
  */
 
 if (! class_exists(PHPUnit\Framework\Error\Deprecated::class)) {
