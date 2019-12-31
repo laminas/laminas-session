9bf5f42f8ccc0a7bdd6d62b8c824475edd2c654a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:45:55 -0600

    Rewriting as Laminas Project package

--- test/Service/SessionManagerFactoryTest.php
+++ test/Service/SessionManagerFactoryTest.php
@@ -1,41 +1,40 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-session for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-session/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-session/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Session\Service;
+namespace LaminasTest\Session\Service;
 
-use Zend\ServiceManager\ServiceManager;
-use Zend\Session\Container;
-use Zend\Session\Service\SessionManagerFactory;
-use Zend\Session\Storage\ArrayStorage;
+use Laminas\ServiceManager\ServiceManager;
+use Laminas\Session\Container;
+use Laminas\Session\Service\SessionManagerFactory;
+use Laminas\Session\Storage\ArrayStorage;
 
 /**
- * @group      Zend_Session
+ * @group      Laminas_Session
  */
 class SessionManagerFactoryTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
         $this->services = new ServiceManager();
-        $this->services->setFactory('Zend\Session\ManagerInterface', 'Zend\Session\Service\SessionManagerFactory');
+        $this->services->setFactory('Laminas\Session\ManagerInterface', 'Laminas\Session\Service\SessionManagerFactory');
     }
 
     public function testCreatesSessionManager()
     {
-        $manager = $this->services->get('Zend\Session\ManagerInterface');
-        $this->assertInstanceOf('Zend\Session\SessionManager', $manager);
+        $manager = $this->services->get('Laminas\Session\ManagerInterface');
+        $this->assertInstanceOf('Laminas\Session\SessionManager', $manager);
     }
 
     public function testConfigObjectIsInjectedIfPresentInServices()
     {
-        $config = $this->getMock('Zend\Session\Config\ConfigInterface');
-        $this->services->setService('Zend\Session\Config\ConfigInterface', $config);
-        $manager = $this->services->get('Zend\Session\ManagerInterface');
+        $config = $this->getMock('Laminas\Session\Config\ConfigInterface');
+        $this->services->setService('Laminas\Session\Config\ConfigInterface', $config);
+        $manager = $this->services->get('Laminas\Session\ManagerInterface');
         $test = $manager->getConfig();
         $this->assertSame($config, $test);
     }
@@ -44,24 +43,24 @@ class SessionManagerFactoryTest extends \PHPUnit_Framework_TestCase
     {
         // Using concrete version here as mocking was too complex
         $storage = new ArrayStorage();
-        $this->services->setService('Zend\Session\Storage\StorageInterface', $storage);
-        $manager = $this->services->get('Zend\Session\ManagerInterface');
+        $this->services->setService('Laminas\Session\Storage\StorageInterface', $storage);
+        $manager = $this->services->get('Laminas\Session\ManagerInterface');
         $test = $manager->getStorage();
         $this->assertSame($storage, $test);
     }
 
     public function testFactoryWillInjectSaveHandlerIfPresentInServices()
     {
-        $saveHandler = $this->getMock('Zend\Session\SaveHandler\SaveHandlerInterface');
-        $this->services->setService('Zend\Session\SaveHandler\SaveHandlerInterface', $saveHandler);
-        $manager = $this->services->get('Zend\Session\ManagerInterface');
+        $saveHandler = $this->getMock('Laminas\Session\SaveHandler\SaveHandlerInterface');
+        $this->services->setService('Laminas\Session\SaveHandler\SaveHandlerInterface', $saveHandler);
+        $manager = $this->services->get('Laminas\Session\ManagerInterface');
         $test = $manager->getSaveHandler();
         $this->assertSame($saveHandler, $test);
     }
 
     public function testFactoryWillMarkManagerAsContainerDefaultByDefault()
     {
-        $manager = $this->services->get('Zend\Session\ManagerInterface');
+        $manager = $this->services->get('Laminas\Session\ManagerInterface');
         $this->assertSame($manager, Container::getDefaultManager());
     }
 
@@ -71,7 +70,7 @@ class SessionManagerFactoryTest extends \PHPUnit_Framework_TestCase
             'enable_default_container_manager' => false,
         ));
         $this->services->setService('Config', $config);
-        $manager = $this->services->get('Zend\Session\ManagerInterface');
+        $manager = $this->services->get('Laminas\Session\ManagerInterface');
         $this->assertNotSame($manager, Container::getDefaultManager());
     }
 
@@ -82,11 +81,11 @@ class SessionManagerFactoryTest extends \PHPUnit_Framework_TestCase
     {
         $config = array('session_manager' => array(
             'validators' => array(
-                'Zend\Session\Validator\RemoteAddr',
+                'Laminas\Session\Validator\RemoteAddr',
             ),
         ));
         $this->services->setService('Config', $config);
-        $manager = $this->services->get('Zend\Session\ManagerInterface');
+        $manager = $this->services->get('Laminas\Session\ManagerInterface');
 
         $manager->start();
 
@@ -99,9 +98,9 @@ class SessionManagerFactoryTest extends \PHPUnit_Framework_TestCase
     public function testStartingSessionManagerFromFactoryDoesNotTriggerUndefinedVariable()
     {
         $storage = new ArrayStorage();
-        $this->services->setService('Zend\Session\Storage\StorageInterface', $storage);
+        $this->services->setService('Laminas\Session\Storage\StorageInterface', $storage);
 
-        $manager = $this->services->get('Zend\Session\ManagerInterface');
+        $manager = $this->services->get('Laminas\Session\ManagerInterface');
         $manager->start();
 
         $this->assertSame($storage, $manager->getStorage());
@@ -114,27 +113,27 @@ class SessionManagerFactoryTest extends \PHPUnit_Framework_TestCase
     {
         $storage = new ArrayStorage();
         $storage->setMetadata('_VALID', array(
-            'Zend\Session\Validator\HttpUserAgent' => 'Foo',
-            'Zend\Session\Validator\RemoteAddr'    => '1.2.3.4',
+            'Laminas\Session\Validator\HttpUserAgent' => 'Foo',
+            'Laminas\Session\Validator\RemoteAddr'    => '1.2.3.4',
         ));
-        $this->services->setService('Zend\Session\Storage\StorageInterface', $storage);
+        $this->services->setService('Laminas\Session\Storage\StorageInterface', $storage);
 
         $config = array(
             'session_manager' => array(
                 'validators' => array(
-                    'Zend\Session\Validator\HttpUserAgent',
-                    'Zend\Session\Validator\RemoteAddr',
+                    'Laminas\Session\Validator\HttpUserAgent',
+                    'Laminas\Session\Validator\RemoteAddr',
                 ),
             ),
         );
         $this->services->setService('Config', $config);
 
         // This call is needed to make sure session storage data is not overwritten by the factory
-        $manager = $this->services->get('Zend\Session\ManagerInterface');
+        $manager = $this->services->get('Laminas\Session\ManagerInterface');
 
         $validatorData = $storage->getMetaData('_VALID');
-        $this->assertSame('Foo', $validatorData['Zend\Session\Validator\HttpUserAgent']);
-        $this->assertSame('1.2.3.4', $validatorData['Zend\Session\Validator\RemoteAddr']);
+        $this->assertSame('Foo', $validatorData['Laminas\Session\Validator\HttpUserAgent']);
+        $this->assertSame('1.2.3.4', $validatorData['Laminas\Session\Validator\RemoteAddr']);
     }
 
     /**
@@ -144,20 +143,20 @@ class SessionManagerFactoryTest extends \PHPUnit_Framework_TestCase
     {
         $storage = new ArrayStorage();
         $storage->setMetadata('_VALID', array(
-            'Zend\Session\Validator\RemoteAddr' => '1.2.3.4',
+            'Laminas\Session\Validator\RemoteAddr' => '1.2.3.4',
         ));
-        $this->services->setService('Zend\Session\Storage\StorageInterface', $storage);
+        $this->services->setService('Laminas\Session\Storage\StorageInterface', $storage);
 
         $config = array(
             'session_manager' => array(
                 'validators' => array(
-                    'Zend\Session\Validator\RemoteAddr',
+                    'Laminas\Session\Validator\RemoteAddr',
                 ),
             ),
         );
         $this->services->setService('Config', $config);
 
-        $manager = $this->services->get('Zend\Session\ManagerInterface');
+        $manager = $this->services->get('Laminas\Session\ManagerInterface');
         try {
             $manager->start();
         } catch (\RuntimeException $e) {
