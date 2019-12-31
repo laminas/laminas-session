25811ca7eae3886c6462a14478e1bd8a4594c3f1
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:45:03 -0600

    Rewriting as Laminas Project package

--- src/ValidatorChain.php
+++ src/ValidatorChain.php
@@ -1,23 +1,21 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Session
- */
-namespace Zend\Session;
 
-use Zend\EventManager\EventManager;
-use Zend\Session\Storage\StorageInterface as Storage;
-use Zend\Session\Validator\ValidatorInterface as Validator;
+/**
+ * @see       https://github.com/laminas/laminas-session for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-session/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-session/blob/master/LICENSE.md New BSD License
+ */
+namespace Laminas\Session;
+
+use Laminas\EventManager\EventManager;
+use Laminas\Session\Storage\StorageInterface as Storage;
+use Laminas\Session\Validator\ValidatorInterface as Validator;
 
 /**
  * Validator chain for validating sessions
  *
- * @category   Zend
- * @package    Zend_Session
+ * @category   Laminas
+ * @package    Laminas_Session
  */
 class ValidatorChain extends EventManager
 {
@@ -51,7 +49,7 @@ class ValidatorChain extends EventManager
      * @param  string $event
      * @param  callable $callback
      * @param  int $priority
-     * @return \Zend\Stdlib\CallbackHandler
+     * @return \Laminas\Stdlib\CallbackHandler
      */
     public function attach($event, $callback = null, $priority = 1)
     {
