b472ce0f689b49a44f77a0a4105c991ecdba1fa6
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:45:36 -0600

    Rewriting as Laminas Project package

--- test/Service/SessionConfigFactoryTest.php
+++ test/Service/SessionConfigFactoryTest.php
@@ -1,26 +1,25 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-session for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-session/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-session/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Session\Service;
+namespace LaminasTest\Session\Service;
 
-use Zend\ServiceManager\ServiceManager;
-use Zend\Session\Service\SessionConfigFactory;
+use Laminas\ServiceManager\ServiceManager;
+use Laminas\Session\Service\SessionConfigFactory;
 
 /**
- * @group      Zend_Session
+ * @group      Laminas_Session
  */
 class SessionConfigFactoryTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
         $this->services = new ServiceManager();
-        $this->services->setFactory('Zend\Session\Config\ConfigInterface', 'Zend\Session\Service\SessionConfigFactory');
+        $this->services->setFactory('Laminas\Session\Config\ConfigInterface', 'Laminas\Session\Service\SessionConfigFactory');
     }
 
     public function testCreatesSessionConfigByDefault()
@@ -28,32 +27,32 @@ class SessionConfigFactoryTest extends \PHPUnit_Framework_TestCase
         $this->services->setService('Config', array(
             'session_config' => array(),
         ));
-        $config = $this->services->get('Zend\Session\Config\ConfigInterface');
-        $this->assertInstanceOf('Zend\Session\Config\SessionConfig', $config);
+        $config = $this->services->get('Laminas\Session\Config\ConfigInterface');
+        $this->assertInstanceOf('Laminas\Session\Config\SessionConfig', $config);
     }
 
     public function testCanCreateAlternateSessionConfigTypeViaConfigClassKey()
     {
         $this->services->setService('Config', array(
             'session_config' => array(
-                'config_class' => 'Zend\Session\Config\StandardConfig',
+                'config_class' => 'Laminas\Session\Config\StandardConfig',
             ),
         ));
-        $config = $this->services->get('Zend\Session\Config\ConfigInterface');
-        $this->assertInstanceOf('Zend\Session\Config\StandardConfig', $config);
+        $config = $this->services->get('Laminas\Session\Config\ConfigInterface');
+        $this->assertInstanceOf('Laminas\Session\Config\StandardConfig', $config);
         // Since SessionConfig extends StandardConfig, need to test that it's not that
-        $this->assertNotInstanceOf('Zend\Session\Config\SessionConfig', $config);
+        $this->assertNotInstanceOf('Laminas\Session\Config\SessionConfig', $config);
     }
 
     public function testServiceReceivesConfiguration()
     {
         $this->services->setService('Config', array(
             'session_config' => array(
-                'config_class' => 'Zend\Session\Config\StandardConfig',
-                'name'         => 'zf2',
+                'config_class' => 'Laminas\Session\Config\StandardConfig',
+                'name'         => 'laminas',
             ),
         ));
-        $config = $this->services->get('Zend\Session\Config\ConfigInterface');
-        $this->assertEquals('zf2', $config->getName());
+        $config = $this->services->get('Laminas\Session\Config\ConfigInterface');
+        $this->assertEquals('laminas', $config->getName());
     }
 }
