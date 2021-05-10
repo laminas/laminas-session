bad7936429cd3ae73268d29be77f29ffd9ec1181
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:10:08 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/compatibility/autoload.php
+++ src/compatibility/autoload.php
@@ -1,12 +1,6 @@
 <?php
 
 /**
- * @see       https://github.com/laminas/laminas-session for the canonical source repository
- * @copyright https://github.com/laminas/laminas-session/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-session/blob/master/LICENSE.md New BSD License
- */
-
-/**
  * Legacy purposes only, to prevent code that references it from breaking.
  */
 trigger_error('Polyfill autoload support (file library/Laminas/Session/compatibility/autoload.php) is no longer'
